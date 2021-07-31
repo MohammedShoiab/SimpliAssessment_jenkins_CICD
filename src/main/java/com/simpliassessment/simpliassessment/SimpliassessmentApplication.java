4d0c9335332acd376d00e546c84c7c84e8c88c4e
LENOVO <LENOVO@DESKTOP-0DQDIGG>
2021-07-31 11:11:15 +0530

    Maven-spring boot src

---
+++ src/main/java/com/simpliassessment/simpliassessment/SimpliassessmentApplication.java
@@ -0,0 +1,13 @@
+package com.simpliassessment.simpliassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpliassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpliassessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpliassessment/simpliassessment/SimpliassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpliassessment.simpliassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpliassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
